66d0dbc2d6b437d0af36cca71baf6e5408bf4ae5
yorkyu <yorkyu@tencent.com>
2020-11-07 11:47:21 +0800

    feat: sitemap

--- env.sh
+++ env.sh
@@ -7,3 +7,5 @@ sed -i -e 's@"complex-regexp-analysis\/@'\".\/complex-regexp-analysis\/'@g' ./_b
 sed -i -e 's@"common-regex\/@'\".\/common-regex\/'@g' ./_book/index.html
 sed -i -e 's@"application-scenario\/@'\".\/application-scenario\/'@g' ./_book/index.html
 sed -i -e 's@"advanced\/@'\".\/advanced\/'@g' ./_book/index.html
+
+sed -i -e 's@book.yorkyu.cn\/@'book.yorkyu.cn\/regexp\/'@g' ./_book/sitemap.xml
